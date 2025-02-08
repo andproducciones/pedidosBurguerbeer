36bfd1241f4af11294026c760f9606143e6681d2
Francis Andree Fiallos Silva <fafs.1405@gmail.com>
2025-02-08 02:28:49 -0500

    Update GLOBAL.ts

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,11 +1,5 @@
 export const GLOBAL = {
     url_tienda: 'www.google.com',
-    url: 'http://localhost/burguerbeer/back/',
-    //token_mapbox: 'pk.eyJ1IjoiYW5kcHJvZHVjY2lvbmVzMTk5NCIsImEiOiJjbTNqNDg4cXUwN2lmMm1weXhmYmUwaDdsIn0.J-wcghcsSA8FExXFvyWJrQ',
-    //token_gm: 'AIzaSyAxbB1QDAW1bdyiG0MN0rK7PW3CX5zgIoo'
-    //tallas: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-    //colores: ['Rojo', 'Azul', 'Verde', 'Amarillo', 'Negro', 'Blanco', 'Gris'],
-    //proveedores: ['Temu','Amazon','Ebay'],
-    //almacenes: ['Baños', 'Ambato', 'Quito']
-    //url: 'api.domin.com'
+    url: 'http://localhost/burguerbeer/back/'
+
 }
